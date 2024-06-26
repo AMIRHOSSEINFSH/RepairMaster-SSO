d253dbe31580e804e044ddf3749f0207ef2a094e
Amirhosseinfsh <amirhosseinfsh2000@gmail.com>
2024-06-27 00:04:17 +0330

    grpc of sso done

--- src/main/java/org/webproject/sso/authentication/TokenManagement.kt
+++ src/main/java/org/webproject/sso/authentication/TokenManagement.kt
@@ -1,13 +1,19 @@
 package org.webproject.sso.authentication
 
+import java.time.Instant
 import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.ZoneOffset
 import java.util.*
 
 object TokenManagement {
 
-    data class Token(val lifeTime: Long,val sessionId: UUID,val deviceModel: String,val payload: Any?=null)
-
+    data class Token(val lifeTime: Long,val sessionId: UUID,val deviceModel: String,val payload: Any?=null){
+        fun isTokenExpired(): Boolean {
+            val isNotExpired = LocalDateTime.now().isBefore(LocalDateTime.ofInstant(Instant.ofEpochSecond(lifeTime), ZoneId.systemDefault()))
+            return !isNotExpired
+        }
+    }
     fun generateToken(sessionId: UUID,type: TokenType,deviceModel: String,payload: Any?=null): String {
         val data = Token(LocalDateTime.now().plusSeconds(type.time).toEpochSecond(ZoneOffset.UTC),sessionId,deviceModel,payload)
         return CryptoHelper.encrypt(data)
